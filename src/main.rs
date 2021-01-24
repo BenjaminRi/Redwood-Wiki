b4bd5eb71dde0accf9c7398a40eb62e83bef9ff4
Benjamin <noreply@example.com>
2021-01-24 16:19:51 +0100

    Add id expansion. Add GitHub style markdown CSS.

--- src/main.rs
+++ src/main.rs
@@ -117,6 +117,66 @@ fn rowid_from_str(link_str: &str) -> Option<rowid> {
 	link_str.strip_prefix("id:").map_or(None, |id_str| id_str.parse::<rowid>().ok())
 }
 
+fn expand_id_in_text(text: String, db: &mut Database) -> String {
+	enum ParserState {
+		Init,
+		MatchPrefix1, //i
+		MatchPrefix2, //id
+		MatchRowid(Vec<u8>), //id:
+	}
+	
+	//Add some additional capacity in case we do actually need to expand some IDs
+	let mut str_buf: Vec<u8> = Vec::with_capacity(text.len() + 256);
+	
+	let mut parser_state = ParserState::Init;
+	for ascii_char in text.bytes() {
+		match parser_state {
+			ParserState::Init => {
+				if ascii_char != b'i' {
+					str_buf.push(ascii_char);
+				} else {
+					parser_state = ParserState::MatchPrefix1;
+				}
+			}
+			ParserState::MatchPrefix1 => {
+				if ascii_char != b'd' {
+					str_buf.extend_from_slice(b"i");
+					str_buf.push(ascii_char);
+					parser_state = ParserState::Init;
+				} else {
+					parser_state = ParserState::MatchPrefix2;
+				}
+			}
+			ParserState::MatchPrefix2 => {
+				if ascii_char != b':' {
+					str_buf.extend_from_slice(b"id");
+					str_buf.push(ascii_char);
+					parser_state = ParserState::Init;
+				} else {
+					parser_state = ParserState::MatchRowid(Vec::with_capacity(32));
+				}
+			}
+			ParserState::MatchRowid(mut id_buf) => {
+				if ascii_char < b'0' || ascii_char > b'9' {
+					if let Ok(id) = std::str::from_utf8(&id_buf).unwrap().parse::<rowid>() {
+						let title = db.get_article_title(id).or_else(|| Some("Unknown Article!".to_string())).unwrap();
+						str_buf.extend_from_slice(format!("<a href='../../article/{}'>{}</a>", id, title).as_bytes());
+					} else {
+						str_buf.extend_from_slice(b"id:");
+						str_buf.extend_from_slice(&id_buf);
+					}
+					str_buf.push(ascii_char);
+					parser_state = ParserState::Init;
+				} else {
+					id_buf.push(ascii_char);
+					parser_state = ParserState::MatchRowid(id_buf);
+				}
+			}
+		}
+	}
+	String::from_utf8(str_buf).unwrap() //Note: This should always work, otherwise it's a programmer error
+}
+
 #[tokio::main]
 async fn main() {
 	fern::Dispatch::new()
@@ -195,6 +255,7 @@ async fn article_page(
 	let mut html_generator: Option<ClassedHTMLGenerator> = None;
 
 	let parser = Parser::new_ext(&article_text, options).map(|event| {
+		//println!("Text: {:?}", &event);
 		match event {
 			Event::Start(Tag::CodeBlock(language)) => {
 				let mut syntax = if let CodeBlockKind::Fenced(lang_str) = &language {
@@ -227,7 +288,7 @@ async fn article_page(
 				Event::Html(CowStr::Boxed(html.into_boxed_str()))
 			}
 			Event::Text(text) => {
-				println!("Text: {:?}", &text);
+				//println!("Text: {:?}", &text);
 
 				if let Some(html_generator) = &mut html_generator {
 					// We are in a highlighted code block
@@ -247,6 +308,8 @@ async fn article_page(
 	// Write to String buffer.
 	let mut html_output = String::new();
 	html::push_html(&mut html_output, parser);
+	
+	html_output = expand_id_in_text(html_output, &mut db);
 
 	Ok(warp::reply::html(format!(
 		r#"
@@ -255,6 +318,8 @@ async fn article_page(
 <body>
 
 <style>
+{}
+
 {}
 </style>
 
@@ -271,7 +336,7 @@ async fn article_page(
 </body>
 </html>	
 	"#,
-		css_str, article_number, html_output
+		css_str, github_markdown, article_number, html_output
 	)))
 }
 
@@ -296,3 +361,283 @@ async fn index_page(db: Arc<Mutex<Database>>) -> Result<impl warp::Reply, warp::
 	"#,
 	))
 }
+
+const github_markdown: &str = r####"
+body {
+  font-family: Helvetica, arial, sans-serif;
+  font-size: 14px;
+  line-height: 1.6;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  background-color: white;
+  padding: 30px; }
+
+body > *:first-child {
+  margin-top: 0 !important; }
+body > *:last-child {
+  margin-bottom: 0 !important; }
+
+a {
+  color: #4183C4; }
+a.absent {
+  color: #cc0000; }
+a.anchor {
+  display: block;
+  padding-left: 30px;
+  margin-left: -30px;
+  cursor: pointer;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0; }
+
+h1, h2, h3, h4, h5, h6 {
+  margin: 20px 0 10px;
+  padding: 0;
+  font-weight: bold;
+  -webkit-font-smoothing: antialiased;
+  cursor: text;
+  position: relative; }
+
+h1:hover a.anchor, h2:hover a.anchor, h3:hover a.anchor, h4:hover a.anchor, h5:hover a.anchor, h6:hover a.anchor {
+  background: url("../../images/modules/styleguide/para.png") no-repeat 10px center;
+  text-decoration: none; }
+
+h1 tt, h1 code {
+  font-size: inherit; }
+
+h2 tt, h2 code {
+  font-size: inherit; }
+
+h3 tt, h3 code {
+  font-size: inherit; }
+
+h4 tt, h4 code {
+  font-size: inherit; }
+
+h5 tt, h5 code {
+  font-size: inherit; }
+
+h6 tt, h6 code {
+  font-size: inherit; }
+
+h1 {
+  font-size: 28px;
+  color: black; }
+
+h2 {
+  font-size: 24px;
+  border-bottom: 1px solid #cccccc;
+  color: black; }
+
+h3 {
+  font-size: 18px; }
+
+h4 {
+  font-size: 16px; }
+
+h5 {
+  font-size: 14px; }
+
+h6 {
+  color: #777777;
+  font-size: 14px; }
+
+p, blockquote, ul, ol, dl, li, table, pre {
+  margin: 15px 0; }
+
+hr {
+  background: transparent url("../../images/modules/pulls/dirty-shade.png") repeat-x 0 0;
+  border: 0 none;
+  color: #cccccc;
+  height: 4px;
+  padding: 0; }
+
+body > h2:first-child {
+  margin-top: 0;
+  padding-top: 0; }
+body > h1:first-child {
+  margin-top: 0;
+  padding-top: 0; }
+  body > h1:first-child + h2 {
+    margin-top: 0;
+    padding-top: 0; }
+body > h3:first-child, body > h4:first-child, body > h5:first-child, body > h6:first-child {
+  margin-top: 0;
+  padding-top: 0; }
+
+a:first-child h1, a:first-child h2, a:first-child h3, a:first-child h4, a:first-child h5, a:first-child h6 {
+  margin-top: 0;
+  padding-top: 0; }
+
+h1 p, h2 p, h3 p, h4 p, h5 p, h6 p {
+  margin-top: 0; }
+
+li p.first {
+  display: inline-block; }
+
+ul, ol {
+  padding-left: 30px; }
+
+ul :first-child, ol :first-child {
+  margin-top: 0; }
+
+ul :last-child, ol :last-child {
+  margin-bottom: 0; }
+
+dl {
+  padding: 0; }
+  dl dt {
+    font-size: 14px;
+    font-weight: bold;
+    font-style: italic;
+    padding: 0;
+    margin: 15px 0 5px; }
+    dl dt:first-child {
+      padding: 0; }
+    dl dt > :first-child {
+      margin-top: 0; }
+    dl dt > :last-child {
+      margin-bottom: 0; }
+  dl dd {
+    margin: 0 0 15px;
+    padding: 0 15px; }
+    dl dd > :first-child {
+      margin-top: 0; }
+    dl dd > :last-child {
+      margin-bottom: 0; }
+
+blockquote {
+  border-left: 4px solid #dddddd;
+  padding: 0 15px;
+  color: #777777; }
+  blockquote > :first-child {
+    margin-top: 0; }
+  blockquote > :last-child {
+    margin-bottom: 0; }
+
+table {
+  padding: 0; }
+  table tr {
+    border-top: 1px solid #cccccc;
+    background-color: white;
+    margin: 0;
+    padding: 0; }
+    table tr:nth-child(2n) {
+      background-color: #f8f8f8; }
+    table tr th {
+      font-weight: bold;
+      border: 1px solid #cccccc;
+      text-align: left;
+      margin: 0;
+      padding: 6px 13px; }
+    table tr td {
+      border: 1px solid #cccccc;
+      text-align: left;
+      margin: 0;
+      padding: 6px 13px; }
+    table tr th :first-child, table tr td :first-child {
+      margin-top: 0; }
+    table tr th :last-child, table tr td :last-child {
+      margin-bottom: 0; }
+
+img {
+  max-width: 100%; }
+
+span.frame {
+  display: block;
+  overflow: hidden; }
+  span.frame > span {
+    border: 1px solid #dddddd;
+    display: block;
+    float: left;
+    overflow: hidden;
+    margin: 13px 0 0;
+    padding: 7px;
+    width: auto; }
+  span.frame span img {
+    display: block;
+    float: left; }
+  span.frame span span {
+    clear: both;
+    color: #333333;
+    display: block;
+    padding: 5px 0 0; }
+span.align-center {
+  display: block;
+  overflow: hidden;
+  clear: both; }
+  span.align-center > span {
+    display: block;
+    overflow: hidden;
+    margin: 13px auto 0;
+    text-align: center; }
+  span.align-center span img {
+    margin: 0 auto;
+    text-align: center; }
+span.align-right {
+  display: block;
+  overflow: hidden;
+  clear: both; }
+  span.align-right > span {
+    display: block;
+    overflow: hidden;
+    margin: 13px 0 0;
+    text-align: right; }
+  span.align-right span img {
+    margin: 0;
+    text-align: right; }
+span.float-left {
+  display: block;
+  margin-right: 13px;
+  overflow: hidden;
+  float: left; }
+  span.float-left span {
+    margin: 13px 0 0; }
+span.float-right {
+  display: block;
+  margin-left: 13px;
+  overflow: hidden;
+  float: right; }
+  span.float-right > span {
+    display: block;
+    overflow: hidden;
+    margin: 13px auto 0;
+    text-align: right; }
+
+code, tt {
+  margin: 0 2px;
+  padding: 0 5px;
+  white-space: nowrap;
+  border: 1px solid #eaeaea;
+  background-color: #f8f8f8;
+  border-radius: 3px; }
+
+pre code {
+  margin: 0;
+  padding: 0;
+  white-space: pre;
+  border: none;
+  background: transparent; }
+
+.highlight pre {
+  background-color: #f8f8f8;
+  border: 1px solid #cccccc;
+  font-size: 13px;
+  line-height: 19px;
+  overflow: auto;
+  padding: 6px 10px;
+  border-radius: 3px; }
+
+pre {
+  background-color: #f8f8f8;
+  border: 1px solid #cccccc;
+  font-size: 13px;
+  line-height: 19px;
+  overflow: auto;
+  padding: 6px 10px;
+  border-radius: 3px; }
+  pre code, pre tt {
+    background-color: transparent;
+    border: none; }
+"####;
